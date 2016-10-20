8a586bcf14c9403dc153fe2e6a8fb69e624cbd63
Junwon Lee <jlee@koasys.com>
2016-10-20 20:42:58 +0900

    wsgi 오류 수

--- cross.wsgi.py
+++ cross.wsgi.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import activate_this
+import os
+import sys
+import logging
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(cur_dir)
+logging.basicConfig(stream=sys.stderr)
+
+activate_this = os.path.join(cur_dir, 'env/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
 from cross.app import context as app
 application = app
--- missioncamp.wsgi.py
+++ missioncamp.wsgi.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import activate_this
+import os
+import sys
+import logging
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(cur_dir)
+logging.basicConfig(stream=sys.stderr)
+
+activate_this = os.path.join(cur_dir, 'env/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
 from missioncamp.app import context as app
 application = app
 

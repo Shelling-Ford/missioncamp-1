b0cd6445580ad8df35313aa682a8052a36101c7e
Junwon Lee <jlee@koasys.com>
2016-11-02 13:43:00 +0900

    크로스 wsgi오류 수정

--- cross.wsgi.py
+++ cross.wsgi.py
@@ -9,6 +9,9 @@ sys.path.append(cur_dir)
 logging.basicConfig(stream=sys.stderr)
 
 activate_this = os.path.join(cur_dir, 'env/bin/activate_this.py')
-execfile(activate_this, dict(__file__=activate_this))
+with open(activate_this) as f:
+    code = compile(f.read(), activate_this, 'exec')
+    exec(code)
+
 from cross.app import context as app
 application = app

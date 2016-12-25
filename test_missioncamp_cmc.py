b3b786f9ac0144eae2eac1c5b65e823e5a1c7d38
Junwon Lee <esanzy87@gmail.com>
2016-12-26 07:35:02 +0900

    청대선캠 테스트 파일 주석 수정

--- test_missioncamp_cmc.py
+++ test_missioncamp_cmc.py
@@ -6,19 +6,20 @@ from missioncamp import app as missioncamp
 
 
 class MissioncampCmcTestCase(unittest.TestCase):
+    '''청대선캠 테스트 케이스'''
     def setUp(self):
         missioncamp.APP.config['TESTING'] = True
         self.app = missioncamp.APP.test_client()
-        self.regIndividual()
+        self.reg_individual()
 
     def tearDown(self):
-        self.cancelIndividual()
+        self.cancel_individual()
 
-    def regIndividual(self):
+    def reg_individual(self):
         '''
         개인신청
         '''
-        rv = self.app.post("/cmc/registration", data=dict(
+        result = self.app.post("/cmc/registration", data=dict(
             userid="test123@test.tst",
             pwd="123",
             pwd2="123",
@@ -41,32 +42,28 @@ class MissioncampCmcTestCase(unittest.TestCase):
             language="",
             memo=""
         ), follow_redirects=True)
-        assert '신청이 완료되었습니다.' in rv.data.decode('utf-8')
+        assert '신청이 완료되었습니다.' in result.data.decode('utf-8')
 
-    def cancelIndividual(self):
-        '''
-        신청취소
-        '''
-        rv = self.app.post("/cmc/registration/cancel", data=dict(
+    def cancel_individual(self):
+        '''신청취소'''
+        result = self.app.post("/cmc/registration/cancel", data=dict(
             cancel_reason="테스트"
         ), follow_redirects=True)
-        print(rv.data.decode('utf-8'))
-        assert '신청이 취소되었습니다' in rv.data.decode('utf-8')
+        print(result.data.decode('utf-8'))
+        assert '신청이 취소되었습니다' in result.data.decode('utf-8')
 
-    def testCheckIndividual(self):
-        '''
-        개인신청 조회
-        '''
-        rv = self.app.post("/cmc/login", data=dict(
+    def test_check_individual(self):
+        '''개인신청 조회'''
+        result = self.app.post("/cmc/login", data=dict(
             userid="test123@test.tst",
             pwd="123",
             logintype="개인"
         ), follow_redirects=True)
-        assert '아이디 또는 비밀번호가 잘못되었습니다.' not in rv.data.decode('utf-8')
-        assert '신청 구분을 선택해주세요' not in rv.data.decode('utf-8')
-        assert '아이디를 입력해주세요' not in rv.data.decode('utf-8')
-        assert '비밀번호를 입력해 주세요' not in rv.data.decode('utf-8')
-        assert '개인 신청 조회' in rv.data.decode('utf-8')
+        assert '아이디 또는 비밀번호가 잘못되었습니다.' not in result.data.decode('utf-8')
+        assert '신청 구분을 선택해주세요' not in result.data.decode('utf-8')
+        assert '아이디를 입력해주세요' not in result.data.decode('utf-8')
+        assert '비밀번호를 입력해 주세요' not in result.data.decode('utf-8')
+        assert '개인 신청 조회' in result.data.decode('utf-8')
 
 
 if __name__ == "__main__":

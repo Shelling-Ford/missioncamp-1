d769d9f0cf7596327b3b51d429c2d588bd03ede5
peacekim3 <herbdoctor3@naver.com>
2017-10-25 17:02:34 +0900

    2

--- core/form_config.py
+++ core/form_config.py
@@ -42,8 +42,8 @@ TRAININGS = {
     'ws': [
         ('training1', '시니어비전스쿨'), ('training2', '여성비전스쿨'),
         ('training3', '목회자비전스쿨'), ('training4', '비전스쿨'),
-    //('training5', '월드미션'),
-    //('training6', 'FO'), ('training7', '선교캠프'),  
+    <!--('training5', '월드미션'),-->
+    <!--('training6', 'FO'), ('training7', '선교캠프')-->  
         ('training6', 'FO'), ('training7'),
         ('training8', '어린이/키즈 선교캠프'), ('none', '없음')
     ],
